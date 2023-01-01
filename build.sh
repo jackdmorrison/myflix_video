c9e6c55b60a1899af25fc63f7a68f9b7fc507c24
jack morrison <bobimnot123@gmail.com>
2023-01-01 17:05:24 +0000

    removed react app

--- build.sh
+++
@@ -1,7 +0,0 @@
-docker stop nodeserver 
-docker rm nodeserver 
-cat password.txt | docker login --username jackmoinc --password-stdin
-docker build -t jackmoinc/videoserv:one .
-docker push jackmoinc/videoserv:one
-docker pull jackmoinc/videoserv:one
-docker run -d -it -p 80:80 -p 4000:4000 --name nodeserver jackmoinc/videoserv:one

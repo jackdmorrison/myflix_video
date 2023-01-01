7abb693cc8ed62f4725dd0541033aa1bac162ccd
jack morrison <bobimnot123@gmail.com>
2023-01-01 14:21:21 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cat password.txt | docker login --username jackmoinc --password-stdin
 docker build -t jackmoinc/videoserv:one .
 docker push jackmoinc/videoserv:one
 docker pull jackmoinc/videoserv:one
-docker run -d -it -p 80:80 -p 8080:8080 --name nodeserver jackmoinc/videoserv:one
+docker run -d -it -p 80:80 -p 4000:4000 --name nodeserver jackmoinc/videoserv:one

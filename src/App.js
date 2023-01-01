3666c41f0414cff462ba5b8438c63f9cc32d7946
jack morrison <bobimnot123@gmail.com>
2023-01-01 13:18:16 +0000

    added routine for sending file over port 4000
    also made a dockerfile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+const app = express();
+app.get('/video', (req, res) => {
+  res.sendFile('vids/Test.mp4', { root: __dirname });
+});
+app.listen(4000, () => {
+  console.log('Listening on port 4000!')
+});
+// function App() {
+//   return (
+//     <div className="App">
+//     </div>
+//   );
+// }
 
-export default App;
+//export default App;

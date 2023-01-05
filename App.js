b6b18b9b7198691ac6720c0a152a86c28a8c52cb
jack morrison <bobimnot123@gmail.com>
2023-01-05 10:20:11 +0000

    fixed fetch method, will work now

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
 
+const express = require('express');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const app = express();
 app.get('/video/:id', (req, res) => {
   // res.sendFile('vids/Test.mp4', { root: __dirname });
-  const data = fetch(`http://34.243.107.31/myflix/videos/${req.params.id}`).then((response)=> response.json());
-  vid='vids/'+data.video.video.file;
-  res.sendFile(vid,{root:__dirname});
+  console.log(req.params.id)
+  const dataFetch= async ()=>{
+    const data = await (
+      await fetch(`http://34.243.107.31/myflix/videos/${req.params.id}`)
+    ).json();
+    console.log(data)
+    vid='vids/'+data.video.file;
+    res.sendFile(vid,{root:__dirname});
+  }
+  dataFetch();
+  
+  //vid='vids/'+vidData.video.video.file;
+  //res.sendFile(vid,{root:__dirname});
   
 
   });

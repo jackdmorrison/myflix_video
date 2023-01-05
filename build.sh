af578c9da332d7fa5d19cebd5728b65128709f74
jack morrison <bobimnot123@gmail.com>
2023-01-05 11:16:36 +0000

    added videos

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 docker stop nodeserver 
 docker rm nodeserver 
 cat password.txt | docker login --username jackmoinc --password-stdin
-docker build -t jackmoinc/videoserv:one .
-docker push jackmoinc/videoserv:one
-docker pull jackmoinc/videoserv:one
-docker run -d -it -p 80:80 -p 4000:4000 --name nodeserver jackmoinc/videoserv:one
+docker build -t jackmoinc/videoserv:two .
+docker push jackmoinc/videoserv:two
+docker pull jackmoinc/videoserv:two
+docker run -d -it -p 80:80 -p 4000:4000 --name nodeserver jackmoinc/videoserv:two
